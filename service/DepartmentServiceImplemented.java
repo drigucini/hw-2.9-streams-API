321d79bcb127a6ccb9e13d20e51d63ce12ea8ba0
Sergei <drigaserge@gmail.com>
2023-04-17 17:00:10 +0300

    done

---
+++ service/DepartmentServiceImplemented.java
@@ -0,0 +1,45 @@
+package pro.sky.hwstreams.service;
+
+import org.springframework.stereotype.Service;
+import pro.sky.hwstreams.Employee;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class DepartmentServiceImplemented implements DepartmentService{
+
+    private final EmployeeService employeeService;
+
+    public DepartmentServiceImplemented(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @Override
+    public Optional<Employee> getEmployeeMinSalary(Integer departmentID){
+        Collection<Employee> employees = employeeService.findAll();
+        return employees.stream()
+                .filter(it -> it.getDepartment() == departmentID)
+                .min(Comparator.comparing(Employee::getSalary));
+    }
+    @Override
+    public Optional<Employee> getEmployeeMaxSalary(Integer departmentID){
+        Collection<Employee> employees = employeeService.findAll();
+        return employees.stream()
+                .filter(it -> it.getDepartment() == departmentID)
+                .max(Comparator.comparing(Employee::getSalary));
+    }
+    @Override
+    public List<Employee> getEmployeesByDepartment(Integer departmentID){
+        Collection<Employee> employees = employeeService.findAll();
+        return employees.stream()
+                .filter(it -> it.getDepartment() == departmentID)
+                .collect(Collectors.toList());
+    }
+    @Override
+    public Map<Integer, List<Employee>> getAllEmployeesByDepartments(){
+        Collection<Employee> employees = employeeService.findAll();
+        return employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
+}
